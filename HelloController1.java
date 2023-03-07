c08f0f4249677c11acca899a0c2c2b3ad732ae89
Tetsu <root2120@gmail.com>
2023-03-08 07:54:40 +0900

    Springテスト

---
+++ HelloController1.java
@@ -0,0 +1,27 @@
+package com.example.sample1app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+	  mav.addObject("msg","名前を書いて下さい。");
+	  mav.setViewName("index");
+	  return mav;
+	}
+
+	@PostMapping("/")
+	public ModelAndView form(@RequestParam("text1") String str, 
+	    ModelAndView mav) {
+	  mav.addObject("msg","こんにちは、" + str + "さん！");
+	  mav.addObject("value",str);
+	  mav.setViewName("index");
+	  return mav;
+	}
+}
---
+++ HelloController4.java
@@ -0,0 +1,19 @@
+package com.example.sample1app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+  private boolean flag = false;
+  
+  @RequestMapping("/")
+  public ModelAndView index(ModelAndView mav) {
+    flag = !flag;
+    mav.setViewName("index");
+    mav.addObject("flag", flag);
+    mav.addObject("msg", "サンプルのメッセージです。");
+    return mav;
+  }
+}
