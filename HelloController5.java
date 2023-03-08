f7b9525bc61e0dd38dd86286ed535ea3fc0acbe0
Tetsu <root2120@gmail.com>
2023-03-08 10:09:19 +0900

    Spring練習

---
+++ HelloController5.java
@@ -0,0 +1,18 @@
+package com.example.sample1app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+  
+  @RequestMapping("/")
+  public ModelAndView index(ModelAndView mav) {
+    mav.setViewName("index");
+    mav.addObject("msg", "データを表示します。");
+    String[] data = new String[] {"One","Two","Three"};
+    mav.addObject("data", data);
+    return mav;
+  }
+}
